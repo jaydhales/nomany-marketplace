a1299b8b4708b559450be8881962b52300973cfd
jaydhales <69529548+jaydhales@users.noreply.github.com>
2023-02-28 22:30:48 +0100

    Feat: Init Category and Search Bar Components

---
+++ src/components/CategoryNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategoryNav = () => {
+  return (
+    <div className=" bg-white border-y border-gray-300">
+      <div className="p-5 screen">Vateyr</div>
+    </div>
+  );
+};
+
+export default CategoryNav;
